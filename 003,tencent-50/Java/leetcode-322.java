8b15b544a344fa0330ccca3a66a7900476ea3c3f
Jesselinux <wuchang508@163.com>
2019-08-12 11:51:04 +0800

    003-Dynamic Progremming

---
+++ 003,tencent-50/Java/leetcode-322.java
@@ -0,0 +1,48 @@
+// 动态规划：自下而上
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+
+        int [] f = new int[amount + 1];  // 用一个数组存储
+        f[0] = 0;
+
+        for(int i = 1; i <= amount; i++){
+
+            int cost = Integer.MAX_VALUE;
+
+            for(int j = 0; j < coins.length; j++){
+                if(i - coins[j] >= 0){
+                    if(f[i-coins[j]] != Integer.MAX_VALUE)                    
+                        cost = Math.min(cost, f[i - coins[j]] + 1);
+                }
+            }  
+
+            f[i] = cost;
+        }
+
+        return  f[amount] == Integer.MAX_VALUE? -1 : f[amount];
+    }
+}
+
+// 动态规划：自上而下
+public class Solution {
+
+    public int coinChange(int[] coins, int amount) {        
+        if (amount < 1) return 0;
+        return coinChange(coins, amount, new int[amount]);
+    }
+
+    private int coinChange(int[] coins, int rem, int[] count) {
+        if (rem < 0) return -1;
+        if (rem == 0) return 0;
+        if (count[rem - 1] != 0) return count[rem - 1];
+        int min = Integer.MAX_VALUE;
+        for (int coin : coins) {
+            int res = coinChange(coins, rem - coin, count);
+            if (res >= 0 && res < min)
+                min = 1 + res;
+        }
+        count[rem - 1] = (min == Integer.MAX_VALUE) ? -1 : min;
+        return count[rem - 1];
+    }
+}
+
