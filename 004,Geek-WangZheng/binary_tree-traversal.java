aa4a22fe06c37d488ad8f2ff2caaeadf174084ac
Jesselinux <wuchang508@163.com>
2019-08-20 14:16:36 +0800

    004-binary tree

---
+++ 004,Geek-WangZheng/binary_tree-traversal.java
@@ -0,0 +1,22 @@
+// 二叉树遍历方式伪代码：前序、中序、后序
+void preOrder(Node* root) {
+  if (root == null) return;
+  print root // 此处为伪代码，表示打印 root 节点
+  preOrder(root->left);
+  preOrder(root->right);
+}
+
+void inOrder(Node* root) {
+  if (root == null) return;
+  inOrder(root->left);
+  print root // 此处为伪代码，表示打印 root 节点
+  inOrder(root->right);
+}
+
+void postOrder(Node* root) {
+  if (root == null) return;
+  postOrder(root->left);
+  postOrder(root->right);
+  print root // 此处为伪代码，表示打印 root 节点
+}
+
